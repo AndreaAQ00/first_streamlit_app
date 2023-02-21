cc4b90de6bff98b3fb108ce267bdd81cd1939da1
AndreaAQ00 <125959445+AndreaAQ00@users.noreply.github.com>
2023-02-21 10:17:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header(':avocado::bread:Breakfast Favorites')
+streamlit.header(':avocado::bread: Breakfast Favorites')
 streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
-streamlit.text(':green_salad:Kale, Spinach & Rocket Smoothie')
-streamlit.text(':chicken:Hard-Boiled Free-Range Egg')
-streamlit.text(':avocado::bread: Avocado Toast')
+streamlit.text(':green_salad: Kale, Spinach & Rocket Smoothie')
+streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
+streamlit.text(':avocado: :bread: Avocado Toast')
