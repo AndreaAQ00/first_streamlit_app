dd4beee90b3b9adb0f739df0fc22236b45a5e526
AndreaAQ00 <125959445+AndreaAQ00@users.noreply.github.com>
2023-02-21 09:52:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 
-streamlit.text('emojis:sunglasses Omega 3 & Blueberry Oatmeal')
+streamlit.text(':sunglasses: Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
