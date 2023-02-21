a34332bbd7242b3e28ff384084596221d9b6ea9a
AndreaAQ00 <125959445+AndreaAQ00@users.noreply.github.com>
2023-02-21 09:58:57 +0100

    Update streamlit_app.py
    add emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 
-streamlit.text(':sunglasses: Omega 3 & Blueberry Oatmeal :sunglasses:')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
+streamlit.text(':green_salad:Kale, Spinach & Rocket Smoothie')
+streamlit.text(':chicken:Hard-Boiled Free-Range Egg')
+streamlit.text(':avocado::bread: Avocado Toast')
