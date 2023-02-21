6e448c4290673fb4a895d5963ee2b2e6573a772a
AndreaAQ00 <125959445+AndreaAQ00@users.noreply.github.com>
2023-02-21 10:21:40 +0100

    Update streamlit_app.py
    copy paste emojis instead of adding the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header(':avocado::bread: Breakfast Favorites')
-streamlit.text(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
-streamlit.text(':green_salad: Kale, Spinach & Rocket Smoothie')
-streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
-streamlit.text(':avocado: :bread: Avocado Toast')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
